2f80d0ccb5468f934fb28b2e66cd56a1fdcf9a27
syed Rizvi <rizvi5a@yahoo.com>
2021-05-14 23:45:28 -0500

    added ReadFile, write File and  sendFile functionality

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const fs = require("fs");
-const { write } = require("fs/promises");
+const express =require ("express")
+const path = require('path');
+const app= express();
 
-const DB ='./Develop/db/db.json'
+const PORT = 3000;
 
 
+
+ const DB ='./Develop/db/db.json'
+
+//Works use later
 let mock_data ={
           
-  "title":"Test Title 2",
-  "text":"Test tex 2t"
+  "title":"Test Title 4",
+  "text":"Test tex  4"
 };
+
+readFiles();
 appendFiles(mock_data);
 
-
-
+function readFiles() {
+  fs.readFile(DB, 'utf8' , (err, data) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log("read file method")
+      //string as tested with data(0) undefined
+      console.log(data)
+      
+    })
+  }
 function appendFiles(new_data) {
     fs.readFile(DB, 'utf8' , (err, data) => {
         if (err) {
@@ -28,27 +46,38 @@ function appendFiles(new_data) {
 
        
         parsed_data.push(new_data);
-        writeFiles(parsed_data);
+        writeFile(parsed_data);
 
-        let stringify_data = JSON.stringify(parsed_data);
+        let stringify_data = parsed_data;
         console.log(stringify_data);
         
       })
 
     }
 
-    function writeFiles(content_in_json)
 
-  let json_as_string = JSON.stringify(content_in_json)
+  function writeFile(contents_in_json){
 
-  fs.writeFile('DB', json_as_string, err => {
+  let json_as_string = JSON.stringify(contents_in_json)
+
+  fs.writeFile(DB, json_as_string, err => {
   if (err) {
     console.error(err)
     return
   }
    console.log("File written")
 })
- //function appendFiles() {
-  //   fs.read
-     
-//  }
+}
+
+// app.get ('/', (rq, res) => {
+
+//   console.log(__dirname);
+//   console.log(path);
+//   res.sendFile(path.join(__dirname, 'view.html'))
+
+// })
+
+app.get ('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
